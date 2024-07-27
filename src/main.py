9851b466545fedcdf4abfc85393498b479bf8515
grayd <sharin@you-x.ru>
2024-07-27 14:09:44 +0300

    добавил создание папки

--- src/main.py
+++ src/main.py
@@ -1,11 +1,13 @@
 from src.views import generate_main_page, generate_events_page
 from src.services import get_top_cashback_categories, get_potential_savings
 from src.reports import generate_category_expenses_report, generate_weekday_expenses_report
+import os
 
 if __name__ == '__main__':
     file_path = '../data/operations.xlsx'
 
     # Использование функций
+    os.makedirs('reports', exist_ok=True)
     main_page_data = generate_main_page(file_path)
     events_page_data = generate_events_page(file_path, category='groceries')
 
